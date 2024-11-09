a48566371b72f356bfe1ef2f278e394dfa9f15c5
asyrofilula <asyrofilula@gmail.com>
2024-11-09 13:49:04 +0700

    first commit

---
+++ resources/views/seller/product.blade.php
@@ -0,0 +1,49 @@
+@extends('layout.app')
+
+@section('content')
+
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="row">
+            <div class="col-12">
+                <div class="card mb-4">
+                    <div class="card-header d-flex justify-content-between align-items-center">
+                        <h5 class="mb-0">Product List</h5>
+                        <a href="{{ route('product.create') }}" class="btn btn-primary">Add Product</a>
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($products as $product)
+                                    <tr>
+                                        <th scope="row">{{ $product->id }}</th>
+                                        <td>{{ $product->name }}</td>
+                                        <td>{{ $product->description }}</td>
+                                        <td>{{ $product->price }}</td>
+                                        <td>
+                                            <a href="{{ route('product.edit', $product->id) }}" class="btn btn-primary">Edit</a>
+                                            <form action="{{ route('product.destroy', $product->id) }}" method="POST" class="d-inline">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
